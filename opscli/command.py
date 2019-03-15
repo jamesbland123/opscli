0ac03db089d14eaea1a89dae9a34800f1de433e2
jamesbland123 <james@teclait.com>
2019-03-15 09:20:54 -0700

    added try except block to improve error messaging

--- opscli/command.py
+++ opscli/command.py
@@ -14,13 +14,24 @@ class Command():
             print('Unrecognized command')
             parser.print_help()
             sys.exit(1)
-            
+
         if args.command:
             result = getattr(self, args.command)()
             print(result)
 
     def get(self):
-        mod = importlib.import_module('.account', package='opscli.provider')
+        parser = argparse.ArgumentParser(description='Provider service subcommand')
+        parser.add_argument('provider')
+        args = parser.parse_args(sys.argv[2:3])
+
+        try: 
+            mod = importlib.import_module('.' + args.provider, package='opscli.provider')
+
+        except ImportError as error:
+            print("Error: ", error.msg)
+            parser.print_help()
+            sys.exit(1)
+
         function = getattr(mod, "Account")
         instance_of_class = function()
         return instance_of_class.get()
